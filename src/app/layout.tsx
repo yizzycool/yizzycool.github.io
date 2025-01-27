c235e38115af250da42347c77774bd74bc544b3c
yizzycool <chsh110768@gmail.com>
2025-01-27 21:35:44 +0800

    add: font family - Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="bg-white text-neutral-700 antialiased dark:bg-neutral-900 dark:text-neutral-300">
         <Header />
         {children}
