3476961a8b1aa6811aa92d106fce4279105db29e
yizzycool <chsh110768@gmail.com>
2025-01-27 21:35:13 +0800

    update: eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      'next',
+      'next/typescript',
+      'prettier'
+    ],
+    rules: {
+      'react/react-in-jsx-scope': 'off'
+    },
+  }),
+];
+
+export default eslintConfig;
