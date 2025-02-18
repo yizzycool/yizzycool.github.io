31f521d80b6b0c077b41ea1e1d8c22a1530c038a
yizzycool <chsh110768@gmail.com>
2025-02-18 22:08:55 +0800

    add: gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import 'animate.css';
+import Script from 'next/script';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 
@@ -21,8 +22,25 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           rel="stylesheet"
         />
+        <Script id="gtm" strategy="lazyOnload">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TVFKTXLV');
+          `}
+        </Script>
       </head>
       <body className="bg-white text-neutral-700 antialiased dark:bg-neutral-900 dark:text-neutral-300">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TVFKTXLV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
         <Footer />
