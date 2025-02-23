40678244002ef5f5cfbcc977ea8ec47d996b4fef
yizzycool <chsh110768@gmail.com>
2025-02-23 15:39:34 +0800

    add new page: tools/chrome-built-in-ai-api/prompt

---
+++ src/app/tools/chrome-built-in-ai-api/prompt/page.tsx
@@ -0,0 +1,3 @@
+import PromptApi from '@/components/tools/chrome-built-in-ai-api/prompt-api';
+
+export default PromptApi;
---
+++ src/components/tools/chrome-built-in-ai-api/prompt-api/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Title from '../../components/title';
+import SupportTable from '../components/support-table';
+
+export default function PromptApi() {
+  return (
+    <div className="mx-auto max-w-screen-2xl pt-[68px] text-center">
+      <Title>Gemini Nano (Prompt API)</Title>
+      <SupportTable />
+    </div>
+  );
+}
