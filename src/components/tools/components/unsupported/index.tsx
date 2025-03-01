72d09ce59a011d929557504f3c1b43712680a1f1
yizzycool <chsh110768@gmail.com>
2025-03-01 14:51:21 +0800

    update: use X icon for unsupported API, and Alert icon for unstable API

--- src/components/tools/components/unsupported/index.tsx
+++ src/components/tools/components/unsupported/index.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { OctagonX } from 'lucide-react';
+import { OctagonAlert, OctagonX } from 'lucide-react';
 import _get from 'lodash/get';
 
 export const TypeInfo = {
   unsupported: {
     title: 'Unsupported Feature',
     desc: 'This feature is not supported on your device at the moment. Please try again later.',
+    icon: <OctagonX className="h-full w-full text-red-500" />,
   },
   partialUnsupported: {
     title: 'Unstable API',
     desc: 'Although your device supports this API, an error occurred during the creation process. This might be because the API is still in the development stage and remains unstable on some devices. Please try again later!',
+    icon: <OctagonAlert className="h-full w-full text-yellow-500" />,
   },
 };
 
@@ -19,9 +21,11 @@ export type UnsupportedTypes = keyof typeof TypeInfo;
 export default function Unsupported({ type }: { type: UnsupportedTypes }) {
   return (
     <div className="mx-auto mt-10 max-w-screen-sm px-5 py-20 text-center">
-      <OctagonX className="mx-auto h-20 w-20 text-yellow-500" />
-      <div className="mt-10 text-xl font-bold">{TypeInfo[type].title}</div>
-      <div className="mt-5">{TypeInfo[type].desc}</div>
+      <div className="mx-auto h-20 w-20">{_get(TypeInfo, `${type}.icon`)}</div>
+      <div className="mt-10 text-xl font-bold">
+        {_get(TypeInfo, `${type}.title`)}
+      </div>
+      <div className="mt-5">{_get(TypeInfo, `${type}.desc`)}</div>
     </div>
   );
 }
