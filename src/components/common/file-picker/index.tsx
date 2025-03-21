354c642fb9676d78765ba2e5fe062c281581e965
yizzy_wu <yizzy_wu@perfectcorp.com>
2025-03-21 16:44:36 +0800

    udpate: support drag-and-drop for file-picker component

--- src/components/common/file-picker/index.tsx
+++ src/components/common/file-picker/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo, useRef } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { FilePlus, ImagePlus } from 'lucide-react';
 
 export default function FilePicker({
@@ -10,6 +10,8 @@ export default function FilePicker({
   type: string;
   onFileChange: (file: File) => void;
 }) {
+  const [isDragging, setIsDragging] = useState(false);
+
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const Icon = useMemo(() => {
@@ -26,12 +28,12 @@ export default function FilePicker({
     return 'Choose a file';
   }, [type]);
 
-  // const hint = useMemo(() => {
-  //   if (type === 'image') {
-  //     return 'or drop an image here';
-  //   }
-  //   return '';
-  // }, [type]);
+  const hint = useMemo(() => {
+    if (type === 'image') {
+      return 'or drop an image here';
+    }
+    return '';
+  }, [type]);
 
   const accept = useMemo(() => {
     if (type === 'image') {
@@ -47,16 +49,44 @@ export default function FilePicker({
     onFileChange(file);
   };
 
+  const onDragEnter = (event: React.DragEvent<HTMLButtonElement>) => {
+    cancelDefault(event);
+    setIsDragging(true);
+  };
+
+  const onDragLeave = (event: React.DragEvent<HTMLButtonElement>) => {
+    cancelDefault(event);
+    setIsDragging(false);
+  };
+
+  const onDrop = (event: React.DragEvent<HTMLButtonElement>) => {
+    cancelDefault(event);
+    const file = event.dataTransfer.files[0];
+    onFileChange(file);
+    setIsDragging(false);
+  };
+
+  const cancelDefault = (event: React.DragEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  };
+
   return (
     <button
-      className="group mt-8 w-full rounded-lg border-2 border-dashed p-12 text-center"
+      className="group mt-8 w-full rounded-lg border-2 border-dashed p-12 text-center data-[dragging=true]:bg-neutral-500/20 [&_*]:pointer-events-none"
       onClick={() => inputRef.current?.click()}
+      onDragEnter={onDragEnter}
+      onDragOver={cancelDefault}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
+      data-dragging={isDragging}
     >
-      <Icon className="mx-auto h-12 w-12" />
+      <Icon className="pointer-events-none mx-auto h-12 w-12" />
       <div className="mx-auto mt-8 w-fit rounded-full bg-sky-500/80 px-8 py-2 text-white transition-all group-hover:bg-sky-500/90">
         {desc}
       </div>
-      {/* {hint && <div className="mt-4 font-bold">{hint}</div>} */}
+      {hint && <div className="mt-4 font-bold">{hint}</div>}
       <input
         ref={inputRef}
         type="file"
