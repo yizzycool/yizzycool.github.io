dbb192ebe54af042399b15b8a5bbef4270d45507
yizzycool <chsh110768@gmail.com>
2026-01-31 16:34:44 +0800

    update: layout of timestamp converter

--- src/components/tools/dev-tool/unix-timestamp-converter/components/current-time-card/index.tsx
+++ src/components/tools/dev-tool/unix-timestamp-converter/components/current-time-card/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { ClockCheck } from 'lucide-react';
 import Card from '@/components/common/card';
+import CardTitle from '@/components/common/card/title';
 
 type Props = {
   now: Date | null;
@@ -9,27 +11,34 @@ type Props = {
 export default function CurrentTimeCard({ now }: Props) {
   return (
     <Card animation="fade-in">
+      <CardTitle icon={ClockCheck}>Current Device Time</CardTitle>
+
+      {/* Separate */}
+      <div className="-mx-6 my-6 border-b border-neutral-200 dark:border-neutral-700" />
+
       <div className="flex flex-col items-center justify-between gap-8 md:flex-row md:items-start md:gap-4">
         <div className="text-center md:text-left">
           <p className="mb-4 text-sm font-medium text-neutral-500 dark:text-neutral-400">
-            Current Unix Timestamp
+            Local Time
           </p>
-          <h2 className="font-mono text-4xl font-bold tracking-wider text-neutral-900 dark:text-white">
-            {now ? Math.floor(now.getTime() / 1000) : '---'}
+          <h2 className="font-mono text-2xl font-bold tracking-wider text-neutral-900 dark:text-white">
+            {now ? now.toLocaleString() : '---'}
           </h2>
         </div>
         <div className="text-center md:text-right">
           <p className="mb-4 text-sm font-medium text-neutral-500 dark:text-neutral-400">
-            Current UTC Time
-          </p>
-          <p className="font-mono text-lg">{now ? now.toUTCString() : '---'}</p>
-
-          <p className="mb-4 mt-8 text-sm font-medium text-neutral-500 dark:text-neutral-400">
-            Current Local Time
+            Unix Timestamp
           </p>
           <p className="font-mono text-lg">
-            {now ? now.toLocaleString() : '---'}
+            {now ? Math.floor(now.getTime() / 1000) : '---'}
           </p>
+
+          <div className="mt-8" />
+
+          <p className="mb-4 text-sm font-medium text-neutral-500 dark:text-neutral-400">
+            UTC Time
+          </p>
+          <p className="font-mono text-lg">{now ? now.toUTCString() : '---'}</p>
         </div>
       </div>
     </Card>
