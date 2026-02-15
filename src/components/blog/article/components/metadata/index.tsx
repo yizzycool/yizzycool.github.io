bb41accc2821cbb5463274e12d72c5578018312a
yizzycool <chsh110768@gmail.com>
2026-02-15 11:24:29 +0800

    update: remove update time of article

--- src/components/blog/article/components/metadata/index.tsx
+++ src/components/blog/article/components/metadata/index.tsx
@@ -3,7 +3,7 @@
 import type { BlogArticle } from '@/types/blog';
 
 import clsx from 'clsx';
-import { Calendar, Clock, RefreshCw } from 'lucide-react';
+import { Calendar, Clock } from 'lucide-react';
 
 import useGetTransitionClass from '@/hooks/animation/use-get-transition-class';
 import Divider from '@/components/common/divider';
@@ -15,17 +15,10 @@ type Props = { article: BlogArticle };
 
 export default function Metadata({ article }: Props) {
   const data = _get(article, 'data.0') || {};
-  const { updatedAt, publishedAt, createdAt, readTime } = data;
+  const { updatedAt, publishedAt, readTime } = data;
 
-  const publishDate = new Date((publishedAt ?? createdAt) as string);
+  const publishDate = new Date((publishedAt ?? updatedAt) as string);
   const publishDateString = publishDate.toLocaleDateString('en-US', {
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric',
-  });
-
-  const updateDate = new Date(updatedAt as string);
-  const updateDateString = updateDate.toLocaleDateString('en-US', {
     month: 'short',
     day: 'numeric',
     year: 'numeric',
@@ -52,13 +45,6 @@ export default function Metadata({ article }: Props) {
       <time className="flex items-center gap-2">
         <Calendar size={14} />
         <span>{publishDateString}</span>
-      </time>
-
-      <Divider orientation="vertical" className="my-1" />
-
-      <time className="flex items-center gap-2 italic">
-        <RefreshCw size={14} />
-        <span>{updateDateString}</span>
       </time>
     </div>
   );
